48ea8c62ed7652ba0c31e9a6fb15fd2e1b821107
Lewis <ljoncb@gmail.com>
2024-04-02 17:09:47 +0100

    feat: initial scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import './App.css'
-import PageTitle from './components/PageTitle'
-import StoreNav from './components/StoreNav'
-import gryphon from './assets/gryphon.jpg'
-import dd2 from './assets/dd2.jpg'
+import "./App.css";
+import PageTitle from "./components/PageTitle";
+import StoreNav from "./components/StoreNav";
+import gryphon from "./assets/gryphon.jpg";
+import dd2 from "./assets/dd2.jpg";
+import { ChangeEvent, useState } from "react";
+
+const images = [
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+  gryphon,
+];
+
+const perPage = 5;
 
 function App() {
+  const [offset, setOffset] = useState(0);
+  const totalImageWidth = 116 + 2;
+  const totalWidth = totalImageWidth * images.length;
+  const topOffset = totalWidth - (perPage + 1) * totalImageWidth;
+  const onScroll = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setOffset(((Number(value) * topOffset) / 100) * -1);
+  };
+
   return (
     <div className="container">
       <main>
@@ -12,70 +37,70 @@ function App() {
 
         <PageTitle />
 
-        <article className='game-highlights'>
-          <section className='left-col'>
-            <div className='highlight'>
+        <article className="game-highlights">
+          <section className="left-col">
+            <div className="highlight">
               <img src={gryphon} />
             </div>
-            <div className='highlight-strip'>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
+            <div className="highlight-strip-container">
+              <div className="highlight-strip" style={{ left: offset }}>
+                {images.map((image, index) => (
+                  <div
+                    key={`highlight-${index}`}
+                    className="highlight-strip-element"
+                  >
+                    <img src={image} />
+                  </div>
+                ))}
               </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
-              <div className='highlight-strip-element'>
-                <img src={gryphon} />
-              </div>
+            </div>
+            <div className="highlight-slider">
+              <div className="btn btn-left"></div>
+              <input
+                type="range"
+                className="highlight-slider-track"
+                onChange={(e) => onScroll(e)}
+                defaultValue={offset}
+              />
+              <div className="btn btn-right"></div>
             </div>
             <div></div>
           </section>
-          <section className='right-col'>
-            <div className='game-image'>
-              <img src={dd2}/>
+          <section className="right-col">
+            <div className="game-image">
+              <img src={dd2} />
             </div>
-            <div className='game-description'>
-              Dragon’s Dogma 2 is a single player, narrative driven action-RPG that challenges the players to choose their own experience – from the appearance of their Arisen, their vocation, their party, how to approach different situations and more - in a truly immersive fantasy world. 
+            <div className="game-description">
+              Dragon’s Dogma 2 is a single player, narrative driven action-RPG
+              that challenges the players to choose their own experience – from
+              the appearance of their Arisen, their vocation, their party, how
+              to approach different situations and more - in a truly immersive
+              fantasy world.
             </div>
-            <div className='glance'>
-              <div className='subtitle'>
+            <div className="glance">
+              <div className="subtitle">
                 <div>ALL REVIEWS:</div>
                 <div>RELEASE DATE:</div>
                 <div>DEVELOPER:</div>
                 <div>PUBLISHER:</div>
               </div>
-              <div className='summary'>
+              <div className="summary">
                 <div>Mixed</div>
                 <div>22 Mar, 2024</div>
-                <div><a href="#">CAPCOM Co., Ltd.</a></div>
-                <div><a href="#">CAPCOM Co., Ltd.</a></div>
+                <div>
+                  <a href="#">CAPCOM Co., Ltd.</a>
+                </div>
+                <div>
+                  <a href="#">CAPCOM Co., Ltd.</a>
+                </div>
               </div>
             </div>
             <div></div>
           </section>
         </article>
-
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
